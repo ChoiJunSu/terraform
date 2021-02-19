6e0702e45ec6b603d872876c323b2cae684fcfb0
blue-hope <ms7045436@likelion.org>
2021-02-19 21:52:18 +0900

    kong eb

--- dev/elasticbeanstalk/elasticbeanstalk.tf
+++ dev/elasticbeanstalk/elasticbeanstalk.tf
@@ -57,3 +57,31 @@ resource "aws_elastic_beanstalk_environment" "jja_web_dev" {
     value     = "aws-elasticbeanstalk-ec2-role"
   }
 }
+
+resource "aws_elastic_beanstalk_application" "api_gateway" {
+  name        = "api_gateway"
+  description = "kong gateway"
+}
+
+resource "aws_elastic_beanstalk_environment" "api_gateway_dev" {
+  name                = "api-gateway-dev"
+  application         = aws_elastic_beanstalk_application.api_gateway.name
+  solution_stack_name = "64bit Amazon Linux 2018.03 v2.25.0 running Multi-container Docker 19.03.13-ce (Generic)"
+  tier                = "WebServer"
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = var.vpc_dev_id
+  }
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = join(",", var.vpc_dev_public_subnets)
+  }
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = "aws-elasticbeanstalk-ec2-role"
+  }
+}
